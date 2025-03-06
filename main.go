7bf75e437e7f2a7838dc4801a5c79a81ad668463
rcunov <61329286+rcunov@users.noreply.github.com>
2025-03-06 15:02:34 -0600

    make code a bit more readable

--- main.go
+++ main.go
@@ -57,6 +57,108 @@ func ClearBannedIPs() {
 	resp.Body.Close()
 }
 
+func CheckForBadPeers() {
+	// get list of active torrents
+	requestUrl := qbitBaseUrl + "/api/v2/torrents/info?filter=active"
+	resp, err := client.Get(requestUrl)
+	if err != nil {
+		logger.Error("failed to get active torrents", "error", err.Error(), "status_code", resp.StatusCode)
+		os.Exit(1)
+	}
+	body, _ := io.ReadAll(resp.Body)
+	if string(body) == `[]` {
+		logger.Debug("no active torrents")
+		return
+	}
+
+	// parse active torrents - get hash where state=uploading
+	if !gjson.ValidBytes(body) {
+		logger.Error("invalid json response when attempting to get active torrents")
+		os.Exit(1)
+	}
+	activeTorrents := gjson.ParseBytes(body)
+	uploadingHashes := activeTorrents.Get(`#(state=="uploading")#.hash`)
+	if len(uploadingHashes.Array()) == 0 {
+		logger.Debug("torrents are active but none are uploading")
+		return
+	}
+
+	// get info on uploading torrents
+	var badPeers []peerInfo
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	for _, hash := range uploadingHashes.Array() {
+		wg.Add(1)
+		go func(hash gjson.Result) {
+			defer wg.Done()
+			requestUrl := fmt.Sprintf("%s/api/v2/sync/torrentPeers?hash=%s", qbitBaseUrl, hash)
+			resp, err := client.Get(requestUrl)
+			if err != nil {
+				logger.Error("failed to get torrent by hash", "error", err.Error(), "status_code", resp.StatusCode, "hash", hash.Str)
+				os.Exit(1)
+			}
+			body, _ := io.ReadAll(resp.Body)
+			if resp.StatusCode != http.StatusOK {
+				logger.Error("qbit API returned error when trying to get torrent by hash", "status_code", resp.StatusCode,
+					"response", string(body), "hash", hash.Str,
+				)
+				os.Exit(1)
+			}
+			if !gjson.ValidBytes(body) {
+				logger.Error("invalid json response when attempting to parse uploading torrent", "hash", hash.Str)
+				os.Exit(1)
+			}
+
+			// iterate over each peer and find the ip/port of peers using a blacklisted peer ID
+			details := gjson.ParseBytes(body)
+			peers := details.Get(`peers`)
+			peers.ForEach(func(key, value gjson.Result) bool {
+				peerId := value.Get("peer_id_client").Str
+				if peerId == "-TS0008-" || // torrentstorm (stremio)
+					peerId == "Unknown" || // not sure what these are but they seem sus
+					strings.HasPrefix(peerId, "-WW00") || // webtorrent
+					strings.HasPrefix(peerId, "-LT11") { // elementum
+
+					mu.Lock()
+					badPeers = append(badPeers, peerInfo{Addr: key.String(), Hash: hash.Str, Id: peerId})
+					mu.Unlock()
+				}
+				return true
+			})
+		}(hash)
+	}
+	wg.Wait()
+
+	if len(badPeers) > 0 {
+		logger.Debug("found bad peers", "peers", badPeers)
+	} else {
+		logger.Debug("torrents are uploading but no bad peers found")
+		return
+	}
+
+	// ban them
+	requestUrl = qbitBaseUrl + "/api/v2/transfer/banPeers"
+	addrs := make([]string, len(badPeers))
+	for i, p := range badPeers {
+		addrs[i] = p.Addr
+	}
+	data := url.Values{"peers": {strings.Join(addrs[:], "|")}} // produces {"peers": "1.2.3.4:55|5.6.7.8:99"}
+	resp, err = client.PostForm(requestUrl, data)
+	if err != nil {
+		logger.Error("failed to ban bad peers", "error", err.Error()) // don't include status code because docs say that it always returns a 200 OK
+		os.Exit(1)                                                    // https://github.com/qbittorrent/qBittorrent/wiki/WebUI-API-(qBittorrent-4.1)#ban-peers
+	}
+
+	body, _ = io.ReadAll(resp.Body)
+	if len(body) != 0 {
+		logger.Error("invalid response when trying to ban peers", "peers", badPeers, "response", string(body))
+		os.Exit(1)
+	} else {
+		logger.Info("banned some peers", "peers", badPeers)
+		return
+	}
+}
+
 func main() {
 	InitializeLogging()
 	logger.Info("starting up")
@@ -122,107 +224,8 @@ func main() {
 
 	// start infinite loop here to check for bad peers
 	logger.Info("watching for bad peers")
-mainLoop:
 	for {
+		CheckForBadPeers()
 		time.Sleep(3 * time.Second)
-
-		// get list of active torrents
-		requestUrl = qbitBaseUrl + "/api/v2/torrents/info?filter=active"
-		resp, err = client.Get(requestUrl)
-		if err != nil {
-			logger.Error("failed to get active torrents", "error", err.Error(), "status_code", resp.StatusCode)
-			os.Exit(1)
-		}
-		body, _ = io.ReadAll(resp.Body)
-		if string(body) == `[]` {
-			logger.Debug("no active torrents")
-			continue mainLoop
-		}
-
-		// parse active torrents - get hash where state=uploading
-		if !gjson.ValidBytes(body) {
-			logger.Error("invalid json response when attempting to get active torrents")
-			os.Exit(1)
-		}
-		activeTorrents := gjson.ParseBytes(body)
-		uploadingHashes := activeTorrents.Get(`#(state=="uploading")#.hash`)
-		if len(uploadingHashes.Array()) == 0 {
-			logger.Debug("torrents are active but none are uploading")
-			continue mainLoop
-		}
-
-		// get info on uploading torrents
-		var badPeers []peerInfo
-		var wg sync.WaitGroup
-		var mu sync.Mutex
-		for _, hash := range uploadingHashes.Array() {
-			wg.Add(1)
-			go func(hash gjson.Result) {
-				defer wg.Done()
-				requestUrl := fmt.Sprintf("%s/api/v2/sync/torrentPeers?hash=%s", qbitBaseUrl, hash)
-				resp, err := client.Get(requestUrl)
-				if err != nil {
-					logger.Error("failed to get torrent by hash", "error", err.Error(), "status_code", resp.StatusCode, "hash", hash.Str)
-					os.Exit(1)
-				}
-				body, _ := io.ReadAll(resp.Body)
-				if resp.StatusCode != http.StatusOK {
-					logger.Error("qbit API returned error when trying to get torrent by hash", "status_code", resp.StatusCode,
-						"response", string(body), "hash", hash.Str,
-					)
-					os.Exit(1)
-				}
-				if !gjson.ValidBytes(body) {
-					logger.Error("invalid json response when attempting to parse uploading torrent", "hash", hash.Str)
-					os.Exit(1)
-				}
-
-				// iterate over each peer and find the ip/port of peers using a blacklisted peer ID
-				details := gjson.ParseBytes(body)
-				peers := details.Get(`peers`)
-				peers.ForEach(func(key, value gjson.Result) bool {
-					peerId := value.Get("peer_id_client").Str
-					if peerId == "-TS0008-" || // torrentstorm (stremio)
-						peerId == "Unknown" || // not sure what these are but they seem sus
-						strings.HasPrefix(peerId, "-WW00") || // webtorrent
-						strings.HasPrefix(peerId, "-LT11") { // elementum
-
-						mu.Lock()
-						badPeers = append(badPeers, peerInfo{Addr: key.String(), Hash: hash.Str, Id: peerId})
-						mu.Unlock()
-					}
-					return true
-				})
-			}(hash)
-		}
-		wg.Wait()
-
-		if len(badPeers) > 0 {
-			logger.Debug("found bad peers", "peers", badPeers)
-		} else {
-			logger.Debug("torrents are uploading but no bad peers found")
-			continue mainLoop
-		}
-
-		// ban them
-		requestUrl = qbitBaseUrl + "/api/v2/transfer/banPeers"
-		addrs := make([]string, len(badPeers))
-		for i, p := range badPeers {
-			addrs[i] = p.Addr
-		}
-		data = url.Values{"peers": {strings.Join(addrs[:], "|")}} // produces {"peers": "1.2.3.4:55|5.6.7.8:99"}
-		resp, err = client.PostForm(requestUrl, data)
-		if err != nil {
-			logger.Error("failed to ban bad peers", "error", err.Error()) // don't include status code because docs say that it always returns a 200 OK
-			os.Exit(1)                                                    // https://github.com/qbittorrent/qBittorrent/wiki/WebUI-API-(qBittorrent-4.1)#ban-peers
-		}
-
-		body, _ = io.ReadAll(resp.Body)
-		if len(body) != 0 {
-			logger.Error("invalid response when trying to ban peers", "peers", badPeers, "response", string(body))
-			os.Exit(1)
-		} else {
-			logger.Info("banned some peers", "peers", badPeers)
-		}
 	}
 }
